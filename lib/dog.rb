661803218c8f6b8bcb1e738487dafcbfe01e13e0
Anthony Mai <anthony.mai530@gmail.com>
2019-11-21 01:21:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+
+  def name=(dogs_name)
+    @name = dogs_name
+  end 
+
+  def name
+    @name
+  end 
+  
+end 
+
+class Dog
+  def bark
+    puts "woof!"
+  end 
+  
+end 
